3dd0120f195c139cc49e6aaa60d3219b9befee82
quali1 <examplevxp@gmail.com>
2024-05-25 22:21:40 +0200

    feat: Configure REST framework authentication and permission classes

--- backend/settings.py
+++ backend/settings.py
@@ -35,9 +35,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    "rest_framework.authtoken",
     'hydro',
     'users',
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated"
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- hydro/views.py
+++ hydro/views.py
@@ -1,10 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 class HomePage(APIView):
-    authentication_classes = [JWTAuthentication]
 
     def get(self, request):
         content = {'message': 'Hello, World!'}
