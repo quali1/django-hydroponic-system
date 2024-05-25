e35353014352a7fe641982ab4b0ff85d200723bb
quali1 <examplevxp@gmail.com>
2024-05-25 22:06:52 +0200

    feat: add views for handling user registration, login and logout

---
+++ users/views.py
@@ -0,0 +1,50 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get("username").lower()
+        password = request.POST.get("password").lower()
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid username or password!")
+
+    return render(request, "users/login.html")
+
+
+def register_user(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "An error occurred during Registration")
+
+    context = {"form": form}
+
+    return render(request, "users/register.html", context)
+
+
+@login_required(login_url="home")
+def logout_user(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect("home")
