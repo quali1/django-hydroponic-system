c66508727fc9b3545ab891a3fae3aaf37a63e57c
quali1 <examplevxp@gmail.com>
2024-05-26 18:58:15 +0200

    feat: Add search functionality for Hydroponic Systems

--- hydro/urls.py
+++ hydro/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
-    path('systems/', views.HydroListView.as_view(), name='systems-list'),
-    path('measurements/', views.MeasurementsListView.as_view(), name='measurements-list'),
+    path('systems/', views.hydro_list_view, name='systems-list')
 ]
--- hydro/views.py
+++ hydro/views.py
@@ -2,12 +2,26 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import HydroponicSystem, Measurement
 
-
 # Create your views here.
 
 
 def home_page(request):
     return render(request, 'hydro/home.html')
+
+
+def hydro_list_view(request):
+    search = request.GET.get('search')
+    systems = HydroponicSystem.objects.filter(owner=request.user)
+
+    if search:
+        systems = systems.filter(name__icontains=search)
+
+    context = {
+        'systems': systems,
+        'search': search,
+    }
+
+    return render(request, 'hydro/systems_list.html', context)
 
 
 class HydroListView(ListView):
