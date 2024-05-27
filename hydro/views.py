8a8c46c77c93a1a3cdca006e074207ecd715e95e
quali1 <examplevxp@gmail.com>
2024-05-27 15:16:24 +0200

    feat: Add filtering and sorting Hydroponic Systems

--- hydro/views.py
+++ hydro/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import HydroponicSystem, Measurement
 
+
 # Create your views here.
 
 
@@ -13,8 +14,46 @@ def hydro_list_view(request):
     search = request.GET.get('search')
     systems = HydroponicSystem.objects.filter(owner=request.user)
 
-    if search:
-        systems = systems.filter(name__icontains=search)
+    name = request.GET.get('name')
+    if name:
+        systems = systems.filter(name__icontains=name)
+
+    description = request.GET.get('description')
+    if description:
+        systems = systems.filter(description__icontains=description)
+
+    installation_date = request.GET.get('installation_date')
+    if installation_date:
+        systems = systems.filter(installation_date=installation_date)
+
+    created_at_start = request.GET.get('created_at_start')
+    created_at_end = request.GET.get('created_at_end')
+    if created_at_start and created_at_end:
+        systems = systems.filter(created_at__range=[created_at_start, created_at_end])
+
+    updated_at_start = request.GET.get('updated_at_start')
+    updated_at_end = request.GET.get('updated_at_end')
+    if updated_at_start and updated_at_end:
+        systems = systems.filter(updated_at__range=[updated_at_start, updated_at_end])
+
+    size = request.GET.get('size')
+    if size:
+        systems = systems.filter(size=size)
+
+    system_type = request.GET.get('system_type')
+    if system_type:
+        systems = systems.filter(system_type__icontains=system_type)
+
+    ordering = request.GET.get('ordering')
+    if ordering:
+        if ordering == 'name_asc':
+            systems = systems.order_by('name')
+        elif ordering == 'name_desc':
+            systems = systems.order_by('-name')
+        elif ordering == 'date_asc':
+            systems = systems.order_by('installation_date')
+        elif ordering == 'date_desc':
+            systems = systems.order_by('-installation_date')
 
     context = {
         'systems': systems,
@@ -22,11 +61,6 @@ def hydro_list_view(request):
     }
 
     return render(request, 'hydro/systems_list.html', context)
-
-
-class HydroListView(ListView):
-    model = HydroponicSystem
-    template_name = 'hydro/hydro_list.html'
 
 
 class MeasurementsListView(ListView):
