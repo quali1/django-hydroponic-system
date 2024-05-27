8b66d76f8da0e54de048106f3f218c87ce440b05
quali1 <examplevxp@gmail.com>
2024-05-27 17:18:00 +0200

    fix: Resolve error for unauthorized user accessing page

--- hydro/views.py
+++ hydro/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .filters import MeasurementFilter, HydroponicSystemFilter
 from .models import HydroponicSystem, Measurement
@@ -10,6 +11,7 @@ def home_page(request):
     return render(request, 'hydro/home.html')
 
 
+@login_required(login_url="login")
 def hydro_list_view(request):
     systems = HydroponicSystem.objects.filter(owner=request.user)
     system_filter = HydroponicSystemFilter(request.GET, queryset=systems)
@@ -22,6 +24,7 @@ def hydro_list_view(request):
     return render(request, 'hydro/systems_list.html', context)
 
 
+@login_required(login_url="login")
 def measurements_list_view(request):
     systems = HydroponicSystem.objects.filter(owner=request.user)
     filter = MeasurementFilter(request.GET, queryset=Measurement.objects.all())
